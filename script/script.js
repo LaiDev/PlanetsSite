ade7d8292ee2fa1a953bb18a4d7bcdbd756df914
Aalaizha <l.bnjamin0309@gmail.com>
2022-11-05 18:51:32 -0400

    Improve : Geology marker with nav bar

--- script/script.js
+++ script/script.js
@@ -2,6 +2,12 @@
 let topNav = document.querySelector('.navLinks')
 
 function toggleNavBar(){
+
+    let geologyImg = document.querySelector(".geology");
+    
+geologyImg.style.display = "none"
+
+
     if (topNav.style.display === "none") {
         topNav.style.display = "block";
         } else {
