fa06c10a87f28213cb6345484288ad35fc6704f4
Aalaizha <l.bnjamin0309@gmail.com>
2022-11-05 01:39:31 -0400

    Add Mobile Nav Bar

---
+++ script/script.js
@@ -0,0 +1,10 @@
+
+let topNav = document.querySelector('.navLinks')
+
+function toggleNavBar(){
+    if (topNav.style.display === "none") {
+        topNav.style.display = "block";
+        } else {
+            topNav.style.display = "none";
+          }
+}
