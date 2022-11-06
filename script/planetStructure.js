026ff0fc9558b9161b94ea4706da134ddac5637a
Aalaizha <l.bnjamin0309@gmail.com>
2022-11-06 18:03:59 -0500

    ADD : UI update based on page

--- script/planetStructure.js
+++ script/planetStructure.js
@@ -1,12 +1,14 @@
+let planetDescription = document.querySelector("#planetDescription").childNodes[3];
+
+let hideGeologyMarker = () => {
+//Hides the geology marker
+
+let geologyImg = document.querySelector(".geology");
+    
+geologyImg.style.display = "none"
+}
 
 let mercuryStructure = () => {
-
-
-    //Hides the geology marker
-    
-    let geologyImg = document.querySelector(".geology");
-    
-    geologyImg.style.display = "none"
 
 //Get a reference to the planet image and change it to the one showing the planet structure 
 
@@ -21,5 +23,16 @@ mercuryImg.src = "../assets/planet-mercury-internal.svg";
 let planetDescription = document.querySelector("#planetDescription").childNodes[3];
 
 planetDescription.innerText = "Mercury appears to have a solid silicate crust and mantle overlying a solid, iron sulfide outer core layer, a deeper liquid core layer, and a solid inner core. The planet's density is the second highest in the Solar System at 5.427 g/cm3 , only slightly less than Earth's density."
+
+}
+
+let venusStructure = () => {
+hideGeologyMarker();
+
+let venusImg = document.querySelector("#venusImg");
+
+venusImg.src = "../assets/planet-venus-internal.svg";
+
+planetDescription.innerText = "The similarity in size and density between Venus and Earth suggests they share a similar internal structure: a core, mantle, and crust. Like that of Earth, Venusian core is most likely at least partially liquid because the two planets have been cooling at about the same rate."
 
 }
--- script/planetSurface.js
+++ script/planetSurface.js
@@ -1,12 +1,16 @@
 
+
+let showGeologyMarker = () => {
+    //Makes the geology marker visible
+    
+    let geologyImg = document.querySelector(".geology");
+    
+    geologyImg.style.display = "block";
+    
+}
+
 let mercurySurface = () => {
-
-  //Makes the geology marker visible
-
-  let geologyImg = document.querySelector(".geology");
-
-  geologyImg.style.display = "block";
-
+showGeologyMarker();
 
   let mercuryImg = document.querySelector("#mercuryImg");
 
@@ -14,9 +18,19 @@ let mercurySurface = () => {
 
   //Get a reference to the planet description and changes it to the text about the internal planet structure
 
-  let planetDescription =
-    document.querySelector("#planetDescription").childNodes[3];
+  
 
   planetDescription.innerText =
     "Mercury's surface is similar in appearance to that of the Moon, showing extensive mare-like plains and heavy cratering, indicating that it has been geologically inactive for billions of years. It is more heterogeneous than either Mars's or the Moon’s.";
 };
+
+let venusSurface = () => {
+
+showGeologyMarker();
+
+    let venusImg = document.querySelector("#venusImg");
+
+    venusImg.src = "../assets/planet-venus.svg"
+
+    planetDescription.innerText = "Much of the Venusian surface appears to have been shaped by volcanic activity. Venus has several times as many volcanoes as Earth, and it has 167 large volcanoes that are over 100 km (60 mi) across. The only volcanic complex of this size on Earth is the Big Island of Hawaii."
+}
