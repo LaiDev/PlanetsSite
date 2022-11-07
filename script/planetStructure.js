411c5dff03ab1c15765d103df427ca7c00460927
Aalaizha <l.bnjamin0309@gmail.com>
2022-11-06 22:35:52 -0500

    IMPROVE : Element search

--- script/planetStructure.js
+++ script/planetStructure.js
@@ -1,5 +1,5 @@
 let planetDescription =
-  document.querySelector("#planetDescription").childNodes[3];
+  document.querySelector("#planetDescription p" );
 
 let hideGeologyMarker = () => {
   //Hides the geology marker
@@ -18,8 +18,7 @@ let mercuryStructure = () => {
 
   //Get a reference to the planet description and change it to the text about the internal planet structure
 
-  let planetDescription =
-    document.querySelector("#planetDescription").childNodes[3];
+  
 
   planetDescription.innerText =
     "Mercury appears to have a solid silicate crust and mantle overlying a solid, iron sulfide outer core layer, a deeper liquid core layer, and a solid inner core. The planet's density is the second highest in the Solar System at 5.427 g/cm3 , only slightly less than Earth's density.";
